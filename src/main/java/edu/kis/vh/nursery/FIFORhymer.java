e9bf242f05c765a1da8fa52bbc491cd36b292670
Adam Wysocki <adwyspl@gmail.com>
2021-12-20 11:31:27 +0100

    1.4 stala 3

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,7 +3,8 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
 	public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-	
+
+
 	@Override
 	public int countOut() {
 		while (!callCheck())
